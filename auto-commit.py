105861a64ec4a09ee1daa0cbf7eb29e10e8d13b3
Werner1201 <werner.romling@unigranrio.br>
2020-03-11 17:20:36 -0300

    7797

--- auto-commit.py
+++ auto-commit.py
@@ -20,7 +20,7 @@ def __init__():
 def output_reading(output: list) -> bool:
     teste = isinstance(output, list)
     print(teste)
-    if teste:
+    if teste and output[0] != False:
         for char in output[0]:
             print(char)
             if char == 'M':
